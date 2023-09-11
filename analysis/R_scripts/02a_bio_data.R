602238ef98ed778ed1f40f617cc24d98695c98b0
Kevin See <kevin.see@dfw.wa.gov>
2023-09-11 12:49:43 -0700

    updated the PTAGIS query dates to grab all the Priest tags. So re-ran the 02a_bio_data script and saved the outputs.

--- analysis/R_scripts/02a_bio_data.R
+++ analysis/R_scripts/02a_bio_data.R
@@ -1,7 +1,7 @@
 # Author: Kevin See
 # Purpose: create tag lists to feed to PTAGIS query
 # Created: 8/15/2023
-# Last Modified: 8/21/2023
+# Last Modified: 9/8/2023
 # Notes:
 
 #-----------------------------------------------------------------
@@ -80,72 +80,72 @@ library(here)
 #   relocate(spawn_year,
 #            .before = 0)
 
-# pull out MRR data about all steelhead tags
-bio_df <-
-  sthd_tags |>
-  mutate(ptagis_file_nm = if_else(type == "mark",
-                                  mark_file_name,
-                                  recap_file_name)) |>
-  select(spawn_year,
-         ptagis_file_nm) |>
-  distinct() |>
-  arrange(spawn_year,
-          ptagis_file_nm) |>
-  # slice(1:6) |>
-  mutate(tag_file = map(ptagis_file_nm,
-                        .f = function(x) {
-                          out <-
-                            tryCatch(queryMRRDataFile(x),
-                                     error =
-                                       function(cond) {
-                                         message(paste("Error with file", x))
-                                         message("Error message:")
-                                         message(cond)
-                                         return(NULL)
-                                       },
-                                     warning =
-                                       function(cond) {
-                                         message(paste("Warning with file", x))
-                                         message("Warning message:")
-                                         message(cond)
-                                         return(NULL)
-                                       })
-                          if("spawn_year" %in% names(out)) {
-                            out <- out |>
-                              select(-spawn_year)
-                          }
-
-                          return(out)
-                        })) |>
-  unnest(tag_file) |>
-  # fix problems with one PTAGIS file
-  mutate(
-    across(
-      event_date,
-      ~ if_else(ptagis_file_nm == "TLM-2011-165-PRD.xml",
-                release_date,
-                .))) |>
-  mutate(year = if_else(month(event_date) < 7,
-                              year(event_date),
-                              year(event_date) + 1)) |>
-  relocate(year,
-           .before = 0) |>
-  distinct() |>
-  filter(str_detect(species_run_rear_type, "^3")) |>
-  arrange(year,
-          event_date)
-
-
-bio_df |>
-  filter(pit_tag %in% pit_tag[duplicated(pit_tag)]) |>
-  arrange(pit_tag, event_date) |>
-  select(year,
-         ptagis_file_nm,
-         pit_tag,
-         event_date,
-         event_type) |>
-  slice(11:20)
-
+# # pull out MRR data about all steelhead tags
+# bio_df <-
+#   sthd_tags |>
+#   mutate(ptagis_file_nm = if_else(type == "mark",
+#                                   mark_file_name,
+#                                   recap_file_name)) |>
+#   select(spawn_year,
+#          ptagis_file_nm) |>
+#   distinct() |>
+#   arrange(spawn_year,
+#           ptagis_file_nm) |>
+#   # slice(1:6) |>
+#   mutate(tag_file = map(ptagis_file_nm,
+#                         .f = function(x) {
+#                           out <-
+#                             tryCatch(queryMRRDataFile(x),
+#                                      error =
+#                                        function(cond) {
+#                                          message(paste("Error with file", x))
+#                                          message("Error message:")
+#                                          message(cond)
+#                                          return(NULL)
+#                                        },
+#                                      warning =
+#                                        function(cond) {
+#                                          message(paste("Warning with file", x))
+#                                          message("Warning message:")
+#                                          message(cond)
+#                                          return(NULL)
+#                                        })
+#                           if("spawn_year" %in% names(out)) {
+#                             out <- out |>
+#                               select(-spawn_year)
+#                           }
+#
+#                           return(out)
+#                         })) |>
+#   unnest(tag_file) |>
+#   # fix problems with one PTAGIS file
+#   mutate(
+#     across(
+#       event_date,
+#       ~ if_else(ptagis_file_nm == "TLM-2011-165-PRD.xml",
+#                 release_date,
+#                 .))) |>
+#   mutate(year = if_else(month(event_date) < 7,
+#                               year(event_date),
+#                               year(event_date) + 1)) |>
+#   relocate(year,
+#            .before = 0) |>
+#   distinct() |>
+#   filter(str_detect(species_run_rear_type, "^3")) |>
+#   arrange(year,
+#           event_date)
+#
+#
+# bio_df |>
+#   filter(pit_tag %in% pit_tag[duplicated(pit_tag)]) |>
+#   arrange(pit_tag, event_date) |>
+#   select(year,
+#          ptagis_file_nm,
+#          pit_tag,
+#          event_date,
+#          event_type) |>
+#   slice(11:20)
+#
 
 
 #-----------------------------------------------------------------
@@ -171,9 +171,14 @@ sthd_tags <- read_csv(here("analysis/data/raw_data",
       ~ if_else(event_file_name == "TLM-2011-165-PRD.xml",
                 event_release_date_time_value,
                 .))) |>
-  mutate(spawn_year = if_else(month(event_date_mmddyyyy) < 7,
+  mutate(spawn_year = if_else(month(event_date_mmddyyyy) < 6,
                               year(event_date_mmddyyyy),
                               year(event_date_mmddyyyy) + 1))
+
+# remove tags that are spawning in a future year
+sthd_tags <-
+  sthd_tags |>
+  filter(spawn_year <= year(today()))
 
 # pull out MRR data about all steelhead tags
 bio_df <-
@@ -217,7 +222,7 @@ bio_df <-
       ~ if_else(event_file_name == "TLM-2011-165-PRD.xml",
                 release_date,
                 .))) |>
-  mutate(year = if_else(month(event_date) < 7,
+  mutate(year = if_else(month(event_date) < 6,
                         year(event_date),
                         year(event_date) + 1)) |>
   relocate(year,
@@ -227,6 +232,7 @@ bio_df <-
          str_detect(pit_tag, "\\.\\.\\.", negate = T)) |>
   arrange(year,
           event_date)
+
 
 bio_df |>
   group_by(spawn_year) |>
@@ -262,6 +268,94 @@ bio_df |>
 tabyl(bio_df,
       year,
       species_run_rear_type) |>
+  adorn_totals(where = "both")
+
+
+#-----------------------------------------------------------------
+# add age and final origin data from scales
+scale_age_file <- "T:/DFW-Team FP Upper Columbia Escapement - General/UC_Sthd/inputs/Bio Data/PIT Tag PRD Scale Ages Run Years 2022 to present.xlsx"
+
+scale_age_df <-
+  tibble(sheet_nm = excel_sheets(scale_age_file)) |>
+  mutate(scale_data = map(sheet_nm,
+                          .f = function(x) {
+                            read_excel(scale_age_file,
+                                       sheet = x) |>
+                              clean_names()
+                          })) |>
+  unnest(scale_data) |>
+  filter(spawn_year %in% bio_df$spawn_year) |>
+  mutate(across(age_scales,
+                str_to_title),
+         across(age_scales,
+                ~ if_else(nchar(.) > 10 & !is.na(as.numeric(.)),
+                          as.character(round(as.numeric(.), 1)),
+                          .)))
+
+scale_age_df |>
+  filter(age_scales == "Unreadable") |>
+  filter(primary_pit_tag %in% primary_pit_tag[duplicated(primary_pit_tag)]) |>
+  arrange(spawn_year,
+          primary_pit_tag)
+
+scale_age_df |>
+  filter(!(primary_pit_tag %in% primary_pit_tag[duplicated(primary_pit_tag)] &
+             age_scales == "Unreadable")) |>
+  filter(primary_pit_tag %in% primary_pit_tag[duplicated(primary_pit_tag)]) |>
+  arrange(spawn_year,
+          primary_pit_tag)
+
+setdiff(unique(scale_age_df$ptagis_file_name), unique(bio_df$event_file_name))
+setdiff(unique(bio_df$event_file_name[bio_df$spawn_year %in% unique(scale_age_df$spawn_year)]),
+        unique(scale_age_df$ptagis_file_name))
+
+scale_age_df |>
+  filter(primary_pit_tag %in% bio_df$pit_tag) |>
+  filter(!ptagis_file_name %in% unique(bio_df$event_file_name)) |>
+  select(spawn_year,
+         pit_tag = primary_pit_tag,
+         ptagis_file_name) |>
+  left_join(bio_df |>
+              select(spawn_year,
+                     pit_tag,
+                     event_file_name) |>
+              distinct())
+
+
+
+
+bio_df |>
+  filter(spawn_year %in% c(2022:2023)) |>
+  full_join(scale_age_df |>
+              filter(!(primary_pit_tag %in% primary_pit_tag[duplicated(primary_pit_tag)] &
+                         age_scales == "Unreadable")) |>
+              group_by(primary_pit_tag) |>
+              filter(!is.na(origin_scales)) |>
+              slice(1) |>
+              ungroup() |>
+              mutate(scale_age_data = T) |>
+              select(-c(sheet_nm,
+                        run_year,
+                        ptagis_file_name)),
+            by = join_by(spawn_year,
+                         pit_tag == primary_pit_tag)) |>
+  # names()
+  filter(is.na(scale_age_data)) %>%
+  arrange(event_date,
+          pit_tag) |>
+  select(spawn_year,
+         pit_tag,
+         event_date,
+         event_type,
+         contains("comments"),
+         species_run_rear_type,
+         age_scales,
+         origin_field,
+         origin_scales) |>
+  write_csv(here("outgoing/other/missing_scale_data.csv"))
+  mutate(event_month = month(event_date,
+                             label = T)) |>
+  tabyl(spawn_year, event_month) |>
   adorn_totals(where = "both")
 
 #-----------------------------------------------------------------
@@ -316,6 +410,11 @@ tag_list |>
               delim = '\n',
               col_names = F)
 
+#-----------------------------------------------------------------
+# save biological data for later
+write_rds(bio_df,
+          file = here('analysis/data/derived_data',
+                      paste0('Bio_Data_', min_yr, '_', max_yr, '.rds')))
 
 #-----------------------------------------------------------------
 # decode conditional comments
@@ -351,10 +450,3 @@ bio_df |>
          contains("cwt")) |>
   filter(is.na(cwt_s),
          str_detect(conditional_comments, "CW"))
-
-#-----------------------------------------------------------------
-# save biological data for later
-write_rds(bio_df,
-          file = here('analysis/data/derived_data',
-                      paste0('Bio_Data_', min_yr, '_', max_yr, '.rds')))
-
